c82200a6dd3fee759a8e5e7db7a8c7d4c3021401
Jose J. Fernández <josejfernandez@posteo.dk>
2024-09-03 18:31:27 +0200

    Change line endings to CRLF.

--- AdventOfCode.Solutions/Puzzles/Year2015/Day01/Part1/Alice/WithAggregateFunction.cs
+++ AdventOfCode.Solutions/Puzzles/Year2015/Day01/Part1/Alice/WithAggregateFunction.cs
@@ -1,27 +1,27 @@
-﻿using AdventOfCode.Solutions.Library;
+using AdventOfCode.Solutions.Library;
 using AdventOfCode.Solutions.Library.Metadata;
 
 namespace AdventOfCode.Solutions.Puzzles.Year2015.Day01.Part1.Alice;
 
-    public class WithAggregateFunction : BaseSolution
+public sealed class WithAggregateFunction : BaseSolution
+{
+    public override SolutionMetadata Metadata => new(Year.Year2015, Day.Day01, Part.Part1, Author.Alice);
+
+    public override Task<string> Solve(string input)
     {
-        public override SolutionMetadata Metadata => new(Year.Year2015, Day.Day01, Part.Part1, Author.Alice);
-
-        public override Task<string> Solve(string input)
+        int floor = input.Aggregate(0, (currentFloor, c) =>
         {
-            int floor = input.Aggregate(0, (currentFloor, c) =>
+            if (c == '(')
             {
-                if (c == '(')
-                {
-                    return currentFloor + 1;
-                }
-                else if (c == ')')
-                {
-                    return currentFloor - 1;
-                }
-                return currentFloor;
-            });
+                return currentFloor + 1;
+            }
+            else if (c == ')')
+            {
+                return currentFloor - 1;
+            }
+            return currentFloor;
+        });
 
-            return Task.FromResult(floor.ToString());
-        }
+        return Task.FromResult(floor.ToString());
     }
+}
