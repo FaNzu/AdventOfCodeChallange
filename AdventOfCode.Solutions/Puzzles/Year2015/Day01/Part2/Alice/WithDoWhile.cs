991eb5c04586136ff4a49504e9ff5d144a3f99f2
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-09-15 21:55:30 +0200

    2015 Day 01 part 2

---
+++ AdventOfCode.Solutions/Puzzles/Year2015/Day01/Part2/Alice/WithDoWhile.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode.Solutions.Library;
+using AdventOfCode.Solutions.Library.Metadata;
+
+namespace AdventOfCode.Solutions.Puzzles.Year2015.Day01.Part2.Alice;
+
+public class WithDoWhile : BaseSolution
+{
+    public override SolutionMetadata Metadata => new(Year.Year2015, Day.Day01, Part.Part2, Author.Alice);
+
+    public override Task<string> Solve(string input)
+    {
+        int position = 0;
+        int positionIndex = 0;
+        int floor = 0;
+
+        do
+        {
+            char c = input[positionIndex];
+            position++;
+
+            if (c == '(')
+            {
+                floor++;
+            }
+            else if (c == ')')
+            {
+                floor--;
+            }
+
+            positionIndex++;
+
+        } while (floor != -1 && positionIndex < input.Length);
+
+        return Task.FromResult(position.ToString());
+    }
+}
