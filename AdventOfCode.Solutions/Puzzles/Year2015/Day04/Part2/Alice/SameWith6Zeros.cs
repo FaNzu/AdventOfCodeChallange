4dcd009c61385b72f8732a0c43c0b5d77794da29
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-10-16 20:04:52 +0200

    add day 3 and 4

---
+++ AdventOfCode.Solutions/Puzzles/Year2015/Day04/Part2/Alice/SameWith6Zeros.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using AdventOfCode.Solutions.Library;
+using AdventOfCode.Solutions.Library.Metadata;
+
+namespace AdventOfCode.Solutions.Puzzles.Year2015.Day04.Part2.Alice;
+
+
+
+public sealed class SameWith6Zeros : BaseSolution
+{
+    public override SolutionMetadata Metadata => new(Year.Year2015, Day.Day04, Part.Part2, Author.Alice);
+
+    public override Task<string> Solve(string input)
+    {
+        int number = 0;
+
+        using (var md5 = MD5.Create())
+        {
+            bool found = false;
+
+            while (!found)
+            {
+                string combined = input + number;
+
+                byte[] hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(combined));
+
+                string hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                //Console.WriteLine($"Hash for '{combined}': {hash}");
+
+                if (hash.StartsWith("000000"))
+                {
+                    found = true;
+                }
+                else
+                {
+                    number++;
+                }
+            }
+
+            return Task.FromResult(number.ToString());
+        }
+    }
+    }
